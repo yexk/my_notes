8978a945aa48dda09f3d425c04ae958f81c02df3
yexk_ci <yexk@yexk.cn>
2020-08-23 22:58:04 +0800

    deploy

---
+++ js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d2c5fa33bc16a6bd873a6be5cca49776";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
